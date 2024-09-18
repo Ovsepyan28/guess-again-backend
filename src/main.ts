a4078310df3822585baefa792d70343178b90fc1
Ovsepyan Artem <ovsepyan28@gmail.com>
2024-09-18 16:55:11 +0200

    set env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+// Загрузка переменных окружения из .env файла
+dotenv.config();
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(PORT, () => console.log('Server started on port ' + PORT));
 }
 bootstrap();
